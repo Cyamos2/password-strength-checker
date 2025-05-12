bfebdad817da9bf46d311d65f243ee0ef86f0a05
Chris Amos <71685086+Cyamos2@users.noreply.github.com>
2025-05-12 10:08:49 -0400

    Refactored script: Wrapped checks in function and added main block

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -1,48 +1,41 @@
-password = "CyberSafe123"
-length = len(password)
+def check_password_strength(password):
+    length = len(password)
+    
+    if length >= 12:
+        print("The password is long enough.")
+    else:
+        print("The password is too short.")
 
-if length >= 12:
-    print("The password is long enough.")
-else:
-    print("The password is too short.")
+    has_number = False
+    for char in password:
+        if char.isdigit():
+            has_number = True
+            break
+    if has_number:
+        print("The password contains a number.")
+    else:
+        print("The password does NOT contain a number.")
 
-# Rule #2: Check for at least one number
-has_number = False
+    has_upper = False
+    for char in password:
+        if char.isupper():
+            has_upper = True
+            break
+    if has_upper:
+        print("The password contains an uppercase letter.")
+    else:
+        print("The password does NOT contain an uppercase letter.")
 
-for char in password:
-    if char.isdigit():
-        has_number = True
-        break
+    has_special = False
+    for char in password:
+        if not char.isalnum():
+            has_special = True
+            break
+    if has_special:
+        print("The password contains a special character.")
+    else:
+        print("The password does NOT contain a special character.")
 
-if has_number:
-    print("The password contains a number.")
-else:
-    print("The password does NOT contain a number.")
-
-# Rule #3: Check for at least one uppercase letter
-
-has_upper = False
-
-for char in password:
-    if char.isupper():
-        has_upper = True
-        break
-
-if has_upper:
-    print("The password contains an uppercase letter.")
-else:
-    print("The password does NOT contain an uppercase letter.")
-
-# Rule #4: Check for at least one special character
-
-has_special = False
-
-for car in password:
-    if not char.isalnum():
-        has_special = True
-        break
-
-if has_special:
-    print("The password contains a special character.")
-else:
-    print("The password does NOT contain a special character.")  
+if __name__ == "__main__":
+    test_password = "CyberSafe123"
+    check_password_strength(test_password)
